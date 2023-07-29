6b4310e083aee87ae995a1c0e05b23d3d8182c8d
eugen3199 <3199busi@gmail.com>
2023-07-29 15:17:35 +0630

    update

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -75,7 +75,7 @@
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="/">
+            <a class="nav-link" href="?paginate=10">
               <i class="icon-grid menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
