2d84f0970feb6fa0f7eabccc91ac12d5756800c0
Kaung Htun Thant <3199busi@gmail.com>
2023-08-08 21:33:47 +0630

    Fix i guess

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -69,11 +69,11 @@ class AttendanceController extends Controller
         ]);
 
         $Vtotal = Visitor::get()->count();
-        $Vtoday = Visitor::where('created_at', date('Y-m-d'))
+        $Vtoday = Visitor::where('created_at', '>',date('Y-m-d'))
                     ->count();
 
         $Atotal = Attendance::get()->count();
-        $Atoday = Attendance::where('created_at', '<', date('Y-m-d'))
+        $Atoday = Attendance::where('created_at', '>',date('Y-m-d'))
                     ->groupBy('vis_id')
                     ->count();
 
