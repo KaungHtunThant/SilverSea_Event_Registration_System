f0e7bf121449b600db18b6aa56a70aefec8f2a80
Kaung Htun Thant <3199busi@gmail.com>
2023-08-09 00:28:58 +0630

    Download page update

--- resources/views/admin/visitors/download.blade.php
+++ resources/views/admin/visitors/download.blade.php
@@ -6,8 +6,10 @@
 	<title>Test</title>
 </head>
 <body>
+	<h3>{{ $visitor->conf_id }}</h3>
 	<h3>{{ $visitor->name }}</h3>
 	<h3>{{ $visitor->company }}</h3>
+	<h3>{{ $visitor->phone }}</h3>
 	@php
 		echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG(($visitor->id + 1001) . '-ndn' , 'C128' , 2,50) . '" alt="barcode"/>';
 	@endphp
