4ce6bb5fd08277f01a48984cd727b98eb5d3852a
Kaung Htun Thant <3199busi@gmail.com>
2023-08-13 20:32:29 +0630

    Final Update (Hopefully

--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -149,6 +149,18 @@
             }
         });
     </script>
+    <script type="text/javascript">
+        function checkEmail() {
+            var email = document.getElementById('txtEmail');
+            var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+            if (!filter.test(email.value)) {
+            alert('Please provide a valid email address');
+            email.focus;
+            return false;
+         }
+        }
+    </script>
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
