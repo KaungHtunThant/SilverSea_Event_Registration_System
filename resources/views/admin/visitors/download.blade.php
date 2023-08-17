e7bbc43996ae9dc60d5eb75c284dc0ee14e9970d
Kaung Htun Thant <3199busi@gmail.com>
2023-08-17 16:03:15 +0630

    Download update

--- resources/views/admin/visitors/download.blade.php
+++ resources/views/admin/visitors/download.blade.php
@@ -20,7 +20,7 @@
 
 		.card-bg{
 			width: 2.99in;
-			margin-left: 2.555in;
+			margin-left: 2.405in;
 			padding-top: 100px;
 			background-image: url('{{ url("images/bg.jpg") }}');
 			background-repeat: no-repeat;
