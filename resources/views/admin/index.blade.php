a9b3492aaaaad0efefff06ffcf761b30a7274e71
Kaung Htun Thant <3199busi@gmail.com>
2023-08-26 11:06:14 +0630

    chang

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -123,7 +123,7 @@
                 <div class="card-body">
                     <p class="mb-4">Today’s Visitors</p>
                     <p class="fs-30 mb-2">{{ $Vtoday }}</p>
-                    <p>19-Aug</p>
+                    <p>20-Aug</p>
                 </div>
             </div>
         </div>
